db865bcf051fc50929b4e40c85743f322e6f3644
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-18 12:33:32 +0000

    Setting up webpack and babel

---
+++ ui/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> Hello </div>
+    );
+  }
+}
+
+App.propTypes = {};
+App.defaultProps = {};
+
+export default App;
---
+++ ui/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var config = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+
+
+  }
+};
+
+module.exports = config;
