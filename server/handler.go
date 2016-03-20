4edf4ff5ef06602cd307fc4edc29882136930a3a
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-03-20 19:55:16 +0000

    Adding message handling for channel add

--- server/handler.go
+++ server/handler.go
@@ -8,6 +8,11 @@ import (
 
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
+	var message Message
 	mapstructure.Decode(data, &channel)
 	fmt.Printf("%#v\n", channel)
+	channel.Id = "ABC123"
+	message.Name = "channel add"
+	message.Data = channel
+	client.send <- message
 }
