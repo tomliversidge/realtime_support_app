a093c6b8aa1b8eb0806ab911cc815610dcfee94b
Tom Liversidge <tom@liversidge.me>
2016-03-22 17:29:37 +0000

    Adding stopping

--- server/handler.go
+++ server/handler.go
@@ -5,6 +5,12 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
+const(
+	ChannelStop = iota
+	UserStop
+	MessageStop
+)
+
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
 	err := mapstructure.Decode(data, &channel)
@@ -23,21 +29,34 @@ func addChannel(client *Client, data interface{}) {
 }
 
 func subscribeChannel(client *Client, data interface{}) {
+	stop:= client.NewStopChannel(ChannelStop)
+	result := make(chan r.ChangeResponse)
+	cursor, err := r.Table("channel").
+		Changes(r.ChangesOpts{IncludeInitial: true}).
+		Run(client.session)
+	if err != nil {
+		client.send <- Message{"err", err.Error()}
+	}
 
 	go func() {
-		cursor, err := r.Table("channel").
-			Changes(r.ChangesOpts{IncludeInitial: true}).
-			Run(client.session)
-		if err != nil {
-			client.send <- Message{"err", err.Error()}
-		}
-
 		var change r.ChangeResponse
 		for cursor.Next(&change) {
-			if change.NewValue != nil && change.OldValue == nil {
-				client.send <- Message{"channel add", change.NewValue}
-				fmt.Println("sent channel add message")
-			}
+			result <- change
 		}
 		}()
+
+		go func(){
+			for {
+				select{
+					case <-stop:
+						cursor.Close()
+						return
+					case change := <-result:
+						if change.NewValue != nil && change.OldValue == nil {
+							client.send <- Message{"channel add", change.NewValue}
+							fmt.Println("sent channel add message")
+						}
+				}
+			}
+			}()
 }
